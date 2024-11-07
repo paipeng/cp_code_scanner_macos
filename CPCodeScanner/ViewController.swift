ddce156442ae1301111c2400d9c39f553317203f
Pai Peng <sipaipv6@gmail.com>
2024-11-07 09:22:10 +0800

    Initial Commit

---
+++ CPCodeScanner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CPCodeScanner
+//
+//  Created by Pai Peng on 07.11.24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
