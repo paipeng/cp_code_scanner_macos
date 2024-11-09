0a985ea8cdc84a0b428e8b59a28fd850ce97277f
Pai Peng <sipaipv6@gmail.com>
2024-11-10 06:44:31 +0800

    OOP move code to parent class

--- CPCodeScanner/scanmode/overlay/BaseOverlay.swift
+++ CPCodeScanner/scanmode/overlay/BaseOverlay.swift
@@ -26,4 +26,54 @@ class BaseOverlay : NSView {
     func drawDetectedRects(rects: [NSRect], imageSize: CGSize) {
         
     }
+    
+    
+    
+    func drawClear(rect: NSRect) {
+        guard let ctx = NSGraphicsContext.current?.cgContext else {
+            return
+        }
+        ctx.clear(rect)
+    }
+    
+    func convertDrawRect(detectedRect: NSRect, previewSize: CGSize, imageSize: CGSize) -> NSRect {
+        var imageW: CGFloat = 0
+        var imageH: CGFloat = 0
+        var offsetX: CGFloat = 0
+        var offsetY: CGFloat = 0
+        
+        var factor: CGFloat = 1
+        
+        if imageSize.width/imageSize.height >= previewSize.width/previewSize.height {
+            imageW = previewSize.width
+            imageH = imageW * imageSize.height / imageSize.width
+            
+            
+            offsetY = (previewSize.height - imageH) / 2
+            
+            
+            factor = imageSize.width / imageW
+        } else {
+            imageH = previewSize.height
+            imageW = imageH * imageSize.width / imageSize.height
+
+            offsetX = (previewSize.width - imageW) / 2
+            
+            factor = imageSize.height / imageH
+
+        }
+        
+        print("show image size: \(imageH)- \(imageW) offset: \(offsetX)-\(offsetY) factor: \(factor)")
+        
+        var rect = NSRect()
+        rect.origin.x = offsetX + detectedRect.origin.x / factor
+        rect.origin.y = offsetY + detectedRect.origin.y / factor
+        
+        rect.size.width = detectedRect.size.width / factor
+        rect.size.height = detectedRect.size.height / factor
+        
+        print("convertDrawRect: \(rect) from \(detectedRect)")
+        return rect
+        
+    }
 }
--- CPCodeScanner/scanmode/overlay/QRCodeOverlay.swift
+++ CPCodeScanner/scanmode/overlay/QRCodeOverlay.swift
@@ -68,13 +68,6 @@ class QRCodeOverlay : BaseOverlay {
         
     }
     
-    func drawClear(rect: NSRect) {
-        guard let ctx = NSGraphicsContext.current?.cgContext else {
-            return
-        }
-        ctx.clear(rect)
-    }
-    
     func drawRectangles(rect: NSRect) {
         guard let ctx = NSGraphicsContext.current?.cgContext else {
             return
@@ -96,44 +89,4 @@ class QRCodeOverlay : BaseOverlay {
     }
     
     
-    func convertDrawRect(detectedRect: NSRect, previewSize: CGSize, imageSize: CGSize) -> NSRect {
-        var imageW: CGFloat = 0
-        var imageH: CGFloat = 0
-        var offsetX: CGFloat = 0
-        var offsetY: CGFloat = 0
-        
-        var factor: CGFloat = 1
-        
-        if imageSize.width/imageSize.height >= previewSize.width/previewSize.height {
-            imageW = previewSize.width
-            imageH = imageW * imageSize.height / imageSize.width
-            
-            
-            offsetY = (previewSize.height - imageH) / 2
-            
-            
-            factor = imageSize.width / imageW
-        } else {
-            imageH = previewSize.height
-            imageW = imageH * imageSize.width / imageSize.height
-
-            offsetX = (previewSize.width - imageW) / 2
-            
-            factor = imageSize.height / imageH
-
-        }
-        
-        print("show image size: \(imageH)- \(imageW) offset: \(offsetX)-\(offsetY) factor: \(factor)")
-        
-        var rect = NSRect()
-        rect.origin.x = offsetX + detectedRect.origin.x / factor
-        rect.origin.y = offsetY + detectedRect.origin.y / factor
-        
-        rect.size.width = detectedRect.size.width / factor
-        rect.size.height = detectedRect.size.height / factor
-        
-        print("convertDrawRect: \(rect) from \(detectedRect)")
-        return rect
-        
-    }
 }
