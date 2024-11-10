eb8a5b03455269ef3f025ecd72ea6ee01e66d258
Pai Peng <sipaipv6@gmail.com>
2024-11-10 12:32:55 +0800

    add scan mode and overlay

---
+++ CPCodeScanner/scanmode/HtwmQrScanMode.swift
@@ -0,0 +1,43 @@
+//
+//  HtwmQrScanMode.swift
+//  CPCodeScanner
+//
+//  Created by Pai Peng on 10.11.24.
+//
+
+import Foundation
+import AVFoundation
+import AppKit
+
+
+class HtwmQrScanMode : BaseScanMode {
+    
+    override init(scanModeName: String, overlayRect: NSRect, delegate: ScanModeDelegate) {
+        super.init(scanModeName: scanModeName, overlayRect: overlayRect, delegate: delegate)
+        overlay = HtwmQrOverlay(frame: overlayRect)
+    }
+    
+    override func captureOutput(sampleBuffer: CMSampleBuffer) {
+        super.captureOutput(sampleBuffer: sampleBuffer)
+    }
+    
+    override func handleCapturePhoto(photo: AVCapturePhoto) {
+        super.handleCapturePhoto(photo: photo)
+    }
+    
+    
+    override func decode(image: CIImage) {
+        
+        //get UIImage out of CIImage
+        let qrCodes = Util().decode(ciImage: image)
+        
+        
+        //self.delegate?.decodeQRCode(qrData ?? "no decoded")
+        
+        Task {
+            await MainActor.run { [weak self] in
+                (overlay as! HtwmQrOverlay).drawDetectedQRCodeBounds(qrCodes: qrCodes, imageSize: image.extent.size )
+            }
+        }
+    }
+}
---
+++ CPCodeScanner/scanmode/overlay/HtwmQrOverlay.swift
@@ -0,0 +1,86 @@
+//
+//  HtwmQrOverlay.swift
+//  CPCodeScanner
+//
+//  Created by Pai Peng on 10.11.24.
+//
+
+import Foundation
+
+import AppKit
+
+
+class HtwmQrOverlay : BaseOverlay {
+    var qrCodes: [QrCode]?
+    var imageSize: CGSize = CGSize()
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+
+        //self.wantsLayer = true
+        //self.layer?.backgroundColor = NSColor.green.cgColor       
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func drawDetectedRects(rects: [NSRect], imageSize: CGSize) {
+        super.drawDetectedRects(rects: rects, imageSize: imageSize)
+        self.imageSize = imageSize
+        
+        var qrCodes = [QrCode]()
+        
+        for rect in rects {
+            let qrCode = QrCode(message: "Test", bounds: rect)
+            qrCodes.append(qrCode)
+        }
+        
+        self.drawDetectedQRCodeBounds(qrCodes: qrCodes, imageSize: imageSize)
+    }
+    
+    func drawDetectedQRCodeBounds(qrCodes: [QrCode], imageSize: CGSize) {
+        print("drawDetectedQRCodeBounds : \(qrCodes.count) imageSize: \(imageSize)")
+        self.qrCodes = qrCodes
+        self.imageSize = imageSize
+        
+        
+        self.setNeedsDisplay(self.frame)
+    }
+    
+    
+    override func draw(_ rect: CGRect) {
+        //super.draw(rect)
+        print("draw")
+        guard self.qrCodes != nil && self.qrCodes!.count > 0 else {
+            self.drawClear(rect: rect)
+            return
+        }
+        
+        self.drawRectangles(rect: rect)
+        self.qrCodes!.removeAll()
+        
+    }
+    
+    func drawRectangles(rect: NSRect) {
+        guard let ctx = NSGraphicsContext.current?.cgContext else {
+            return
+        }
+
+        ctx.clear(rect)
+        ctx.saveGState()
+        let color:NSColor = NSColor.red
+        color.set()
+        ctx.setStrokeColor(red: 1, green: 0.5, blue: 0, alpha: 1)
+        ctx.setLineWidth(2)
+        
+        for qrCode in self.qrCodes! {
+            print("qrcode: \(qrCode.message) -> \(qrCode.bounds)")
+            ctx.stroke(self.convertDrawRect(detectedRect: qrCode.bounds!, previewSize: self.frame.size, imageSize: self.imageSize))
+        }
+        defer { ctx.restoreGState() }
+        
+    }
+    
+    
+}
